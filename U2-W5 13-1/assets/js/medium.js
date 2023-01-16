c1d3d8889112ebf4598a34eecf2aa70758317ae0
Luca Scomazzon <scom.luca@gmail.com>
2023-01-16 18:56:20 +0100

    Aggiungo quarto progetto

--- U2-W5 13-1/assets/js/medium.js
+++ U2-W5 13-1/assets/js/medium.js
@@ -1,28 +1,21 @@
-const lettera = document.querySelectorAll("svg g[stroke-linecap='butt']");
-console.log(lettera);
+const m = document.querySelectorAll("svg g[stroke-linecap='butt']");
 const intervallo = setInterval(() => {
-  const randomNumber = Math.floor(Math.random() * lettera.length);
-  const ranLett = lettera[randomNumber];
-  ranLett.getElementsByClassName.transition = "1.2s";
-  if (ranLett.getAttribute("opacity") == 1) {
-    ranLett.setAttribute("opacity", "0");
+  const randomNumber = Math.floor(Math.random() * m.length);
+  const ranM = m[randomNumber];
+  ranM.getElementsByClassName.transition = "1s";
+  if (ranM.getAttribute("opacity") == 1) {
+    ranM.setAttribute("opacity", "0");
   } else {
-    ranLett.setAttribute("opacity", "1");
+    ranM.setAttribute("opacity", "1");
   }
 }, 40);
-
-const fermati = () => {
-  clearInterval(intervallo);
-};
 
 const nav = document.querySelector("nav");
 console.log(nav);
 const li = document.querySelector(".lista-ultima");
 console.log(li);
-
 const header = document.querySelector("header").offsetHeight;
 console.log(header);
-
 window.onscroll = function () {
   if (window.scrollY > header) {
     nav.classList.add("bianca");
